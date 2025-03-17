2bbe211160f9c90a1e2f23829bb9ed31ae1a931c
ƑƦƛЄƬЄƇӇ <fraetech@free.fr>
2025-03-17 23:35:39 +0100

    MAJ 17/03
    Retrait d'un chemin absolu dans une dépendance.

--- functions_anfr.py
+++ functions_anfr.py
@@ -5,14 +5,18 @@ import requests
 import sys
 import os
 
+# On remonte au /home/user pour construire le chemin vers le dossier dim_brest pour les SMS
+h_directory = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+send_sms_path = os.path.join(h_directory, "dim_brest", "sms.py")
+
 def log_message(message, level="INFO"):
     """Fonction de log pour afficher un timestamp avec le niveau d'erreur."""
     timestamp = datetime.now().strftime("%d/%m/%Y à %H:%M:%S")
     print(f"{timestamp} [{level}] -> {message}")
 
 def send_sms(message):
-    script_sms = '/home/pi/dim_brest/sms.py'
-    subprocess.run([sys.executable, script_sms, message])
+    """Exécute le scrpt sms.py avec le message en argument."""
+    subprocess.run([sys.executable, send_sms_path, message])
 
 def get_filename_from_server(url):
     """Récupère le nom du fichier depuis l'URL du serveur."""
